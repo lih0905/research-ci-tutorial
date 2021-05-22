867fe7ba21a7ae694e0f94bc720eeecb0684ee37
Inhwan Lee <lih0905@gmail.com>
2021-05-22 13:26:03 +0000

    unittest

--- main_test.py
+++ main_test.py
@@ -2,10 +2,12 @@ import unittest
 
 import main
 
+
 class MainTest(unittest.TestCase):
     def test_helloworld(self):
         ret = main.helloworld("Test")
         self.assertEqual(ret, "Hello World! TTT!")
-        
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     unittest.main()
